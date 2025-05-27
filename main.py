08e05b45d641208001d284484a9fb8146bb79bd5
Paul Yates <148437369+p-yates@users.noreply.github.com>
2025-05-27 16:16:36 +0200

    change to gemini 2.5 pro

--- main.py
+++ main.py
@@ -64,20 +64,8 @@ class TemplateForecaster(ForecastBot):
     async def run_research(self, question: MetaculusQuestion) -> str:
         async with self._concurrency_limiter:
             research = ""
-            if os.getenv("ASKNEWS_CLIENT_ID") and os.getenv("ASKNEWS_SECRET"):
-                research = await AskNewsSearcher().get_formatted_news_async(
-                    question.question_text
-                )
-            elif os.getenv("EXA_API_KEY"):
-                research = await self._call_exa_smart_searcher(
-                    question.question_text
-                )
-            elif os.getenv("PERPLEXITY_API_KEY"):
+            if os.getenv("PERPLEXITY_API_KEY"):
                 research = await self._call_perplexity(question.question_text)
-            elif os.getenv("OPENROUTER_API_KEY"):
-                research = await self._call_perplexity(
-                    question.question_text, use_open_router=True
-                )
             else:
                 logger.warning(
                     f"No research provider found when processing question URL {question.page_url}. Will pass back empty string."
@@ -358,15 +346,15 @@ if __name__ == "__main__":
         publish_reports_to_metaculus=True,
         folder_to_save_reports_to=None,
         skip_previously_forecasted_questions=True,
-        # llms={  # choose your model names or GeneralLlm llms here, otherwise defaults will be chosen for you
-        #     "default": GeneralLlm(
-        #         model="metaculus/anthropic/claude-3-5-sonnet-20241022",
-        #         temperature=0.3,
-        #         timeout=40,
-        #         allowed_tries=2,
-        #     ),
-        #     "summarizer": "openai/gpt-4o-mini",
-        # },
+         llms={  # choose your model names or GeneralLlm llms here, otherwise defaults will be chosen for you
+             "default": GeneralLlm(
+                 model="google/gemini-2.5-pro-preview",
+                 temperature=0.3,
+                 timeout=40,
+                 allowed_tries=2,
+             ),
+             "summarizer": "openai/gpt-4o-mini",
+         },
     )
 
     if run_mode == "tournament":
